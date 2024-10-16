3e9d2182c174143be2078df52830885dfd8144ce
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2024-10-16 18:51:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Einfache Interaktionen können hier hinzugefügt werden
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded and ready! - But what are you doing here? :)');
+});
