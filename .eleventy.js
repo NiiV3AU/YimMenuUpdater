6f08270ddd5bbe64c8e625bae0e917f0b35ce520
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2025-09-03 12:00:35 +0200

    feat: Rework localization with Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "_site",
+      data: "_data",
+    },
+  };
+};
