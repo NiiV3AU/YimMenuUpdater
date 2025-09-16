525a4297147eb27c6df9f9ddc7abf69260674826
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2025-09-16 22:07:44 +0200

    fix(redirect): implement middleware to finalize server-side language detection

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("functions");
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
