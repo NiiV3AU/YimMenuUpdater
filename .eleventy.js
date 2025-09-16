61f8b6f517c7c3b60e944092eafaa2723f1d6558
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2025-09-16 21:17:20 +0200

    fix(feature): pass _redirects-file through build

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
