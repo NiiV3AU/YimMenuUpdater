c2a5e8ddebfe1f9caa8a74b6cffe1e4806c802e4
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2025-11-26 19:30:11 +0100

    chore: add IndexNow support

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("src/0ed7ff33829e408995319533cc1ef53e.txt");
   eleventyConfig.addPassthroughCopy("functions");
   eleventyConfig.addTransform("htmlmin", function (content) {
     if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
