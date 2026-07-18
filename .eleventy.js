669658d342223cf71d41a86bda8dd1bd0d88b5f1
NV3 <86131759+NiiV3AU@users.noreply.github.com>
2026-07-18 15:56:38 +0200

    fix: show full SHA-256 on desktop, overhaul favicon setup
    - measure available space instead of always truncating the hash
    - replace logo.svg with a full favicon.io icon pack (ico/png/svg/manifest)

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,14 @@ module.exports = async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/llms.txt");
   eleventyConfig.addPassthroughCopy("src/_redirects");
   eleventyConfig.addPassthroughCopy("src/0ed7ff33829e408995319533cc1ef53e.txt");
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("functions");
   eleventyConfig.addTransform("htmlmin", function (content) {
     if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
